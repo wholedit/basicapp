0db1bbf51739e4b1d1e99a2e27dbc73a209b924a
wholedit <wholedit@naver.com>
2026-05-04 15:18:36 +0900

    환경설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { DiAtom } from "react-icons/di";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-slate-500'>
           Edit <code>src/App.js</code> and save to reload.
+          <DiAtom />
         </p>
         <a
           className="App-link"
