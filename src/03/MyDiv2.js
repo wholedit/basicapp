415e466d568da3487459cc7b3df5f3e05de5127f
wholedit <wholedit@naver.com>
2026-05-12 13:55:03 +0900

    Refactor MyDiv components to use props for data passing; update MyDiv1, MyDiv2, and MyDiv3 accordingly

--- src/03/MyDiv2.js
+++ src/03/MyDiv2.js
@@ -1,14 +1,17 @@
 import MyDiv3 from "./MyDiv3"
-export default function MyDiv2() {
-  return (
+// export default function MyDiv2(probs) {
+export default function MyDiv2({dn1, dn2, dn3}) {
+return (
     <div className="flex flex-col p-5 m-10
                     w-3/4 h-3/4
                     justify-center items-center
                     bg-lime-700 text-white">
         <div className="w-full flex justify-start items-center">
-            MyDiv2
+            {/*{`${probs.dn1} > ${probs.dn2}`}*/}
+            {`${dn1} > ${dn2}`}
+            {/*구조분해 할당으로 probs를 {dn1, dn2, dn3}로 바꿔주면 probs.dn1 대신 dn1로 바로 사용할 수 있다.*/}
         </div>
-        <MyDiv3 />
+        <MyDiv3 d1={dn1} d2={dn2} d3={dn3} />
     </div>
   )
 }
--- src/03/MyDiv3.js
+++ src/03/MyDiv3.js
@@ -1,11 +1,11 @@
 
-export default function MyDiv3
-() {
-  return (
+// export default function MyDiv3(probs) {
+export default function MyDiv3({d1, d2, d3}) {  
+    return (
     <div className="flex flex-col p-5 m-10
                     w-3/4 h-3/4
                     bg-lime-500 text-white">
-        MyDiv3
+        {`${d1} > ${d2} > ${d3}`}
 
     </div>
   )
